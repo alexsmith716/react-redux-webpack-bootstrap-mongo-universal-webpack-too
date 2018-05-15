59e7edd9c78d95391b936a15f8dc856ac254ece4
Alex Smith <alexsmith716@outlook.com>
2018-05-15 14:47:29 -0400

    Removed postcss config tester changes and css loader options config path.

--- postcss.config.js
+++ postcss.config.js
@@ -12,35 +12,35 @@
 
 // http://cssnext.io/features/
 
-// module.exports = {
-//   use: [
-//     'postcss-import',
-//     'postcss-url',
-//     'postcss-cssnext',
-//     'postcss-browser-reporter',
-//     'postcss-reporter',
-//   ]
-// };
+module.exports = {
+  use: [
+    'postcss-import',
+    'postcss-url',
+    'postcss-cssnext',
+    'postcss-browser-reporter',
+    'postcss-reporter',
+  ]
+};
 
 
 // https://github.com/postcss/postcss-loader#context-ctx
 
 
-module.exports = ({ file }) => ({
-  plugins: {
-    'postcss-import': { root: file.dirname },
-    'postcss-url': [
-      { filter: './**.*', url: asset => `./${asset.url}` }, // [relative path]
-    ],
-    // http://cssnext.io/usage, http://browserl.ist
-    'postcss-cssnext': {
-      browsers: ['last 2 version'],
-    },
-    // add your "plugins" here
-    // ...
-    // and if you want to compress,
-    // just use css-loader option that already use cssnano under the hood
-    'postcss-browser-reporter': {},
-    'postcss-reporter': {},
-  }
-});
+// module.exports = ({ file }) => ({
+//   plugins: {
+//     'postcss-import': { root: file.dirname },
+//     'postcss-url': [
+//       { filter: './**.*', url: asset => `./${asset.url}` }, // [relative path]
+//     ],
+//     // http://cssnext.io/usage, http://browserl.ist
+//     'postcss-cssnext': {
+//       browsers: ['last 2 version'],
+//     },
+//     // add your "plugins" here
+//     // ...
+//     // and if you want to compress,
+//     // just use css-loader option that already use cssnano under the hood
+//     'postcss-browser-reporter': {},
+//     'postcss-reporter': {},
+//   }
+// });
